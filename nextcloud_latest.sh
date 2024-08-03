2ebb08de1dc394e5f45f887137296398b8f74074
chmodmasx <daninicob@gmail.com>
2024-08-02 22:47:14 -0300

    Update nextcloud_latest.sh
    Nextcloud 28

--- nextcloud_latest.sh
+++ nextcloud_latest.sh
@@ -21,38 +21,34 @@ echo '\n'
 # Pide los datos para la cuenta de administrador
 read -p "Ingresa el nombre de usuario para ingresar a Nextcloud: " NC_ADMIN_USER
 echo '\n'
+stty -echo
 read -p "Ingresa la contraseña para ingresar a Nextcloud: " NC_ADMIN_PASS
+stty echo
 echo '\n'
 
 #Instala los paquetes necesarios
 apt-get update
-apt-get -y install apache2 mariadb-server libapache2-mod-fcgid curl zip unzip php-fpm php-gd php-mysql php-curl php-gmp php-mbstring php-intl php-imagick php-xml php-zip unzip memcached php-memcached redis-server php-redis php-bcmath php-bz2 php-imap php-smbclient php-ldap imagemagick ffmpeg
+apt-get -y install apache2 mariadb-server php libapache2-mod-fcgid libapache2-mod-php curl zip unzip wget php-gd php-mysql php-curl php-gmp php-mbstring php-intl php-imagick php-xml php-zip unzip memcached php-memcached redis-server php-redis php-bcmath php-bz2 php-imap php-smbclient php-ldap imagemagick ffmpeg cron
+
+service apache2 start
+service mariadb start
+service redis-server start
 
 # Descarga y descomprime el archivo zip y mueve la carpeta a /var/www/html/nextcloud
 wget https://download.nextcloud.com/server/releases/latest-27.zip
-sudo unzip latest-27.zip -d /var/www/html/
+unzip latest-27.zip -d /var/www/html/
+rm latest-27.zip
 
 #Obtiene la versión de PHP instalada
 PHP_VERSION=$(php -v | head -n 1 | cut -d " " -f 2 | cut -c 1,2,3)
 
 #Configura Apache
-a2enmod headers
-a2enmod env
-a2enmod dir
-a2enmod mime
-a2enmod proxy_fcgi setenvif
-a2enconf php${PHP_VERSION}-fpm
-a2enmod ssl
-a2ensite default-ssl
+a2enmod headers env dir mime proxy_fcgi setenvif php${PHP_VERSION} ssl
 
-systemctl restart php${PHP_VERSION}-fpm.service
-systemctl restart apache2.service
-
-
-
+service apache2 reload
 
 # Creamos el archivo de configuración de apache para Nextcloud, incluyendo las redirecciones
-sudo echo "<VirtualHost *:80>
+echo "<VirtualHost *:80>
   DocumentRoot /var/www/html/nextcloud/
   ServerName $domain
 
@@ -70,11 +66,16 @@ sudo echo "<VirtualHost *:80>
     </IfModule>
 
   </Directory>
-</VirtualHost>" | sudo tee /etc/apache2/sites-available/nextcloud.conf
+
+    ErrorLog ${APACHE_LOG_DIR}/nextcloud_error.log
+    CustomLog ${APACHE_LOG_DIR}/nextcloud_access.log combined
+</VirtualHost>
+" | tee /etc/apache2/sites-available/nextcloud.conf
 
 
 # Habilitamos el archivo de configuración
 a2ensite nextcloud.conf
+a2dissite 000-default.conf
 a2enmod rewrite
 
 # Configura la base de datos MariaDB
@@ -86,12 +87,13 @@ DB_PASS=$(openssl rand -base64 12)
 DB_NAME="db_$(openssl rand -hex 4)"
 DB_HOST="localhost"
 
-sudo mysql -e "CREATE USER '$DB_USER'@'$DB_HOST' IDENTIFIED BY '$DB_PASS';"
-sudo mysql -e "CREATE DATABASE $DB_USER;"
-sudo mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'$DB_HOST';"
-sudo mysql -e "FLUSH PRIVILEGES;"
+mysql -u root -S /var/run/mysqld/mysqld.sock -e "CREATE USER '$DB_USER'@'$DB_HOST' IDENTIFIED BY '$DB_PASS';"
+mysql -u root -S /var/run/mysqld/mysqld.sock -e "CREATE DATABASE $DB_USER;"
+mysql -u root -S /var/run/mysqld/mysqld.sock -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'$DB_HOST';"
+mysql -u root -S /var/run/mysqld/mysqld.sock -e "FLUSH PRIVILEGES;"
 
-cat <<EOF | sudo tee /var/www/html/nextcloud/config/autoconfig.php
+
+cat <<EOF | tee /var/www/html/nextcloud/config/autoconfig.php
 <?php
 \$AUTOCONFIG = array (
   'dbtype' => 'mysql',
@@ -106,6 +108,7 @@ cat <<EOF | sudo tee /var/www/html/nextcloud/config/autoconfig.php
   'overwriteprotocol' => 'https',
   'overwrite.cli.url' => 'https://$domain',
   'overwritehost' => '$domain',
+  'maintenance_window_start' => 1,
   'enable_previews' => true,
   'enabledPreviewProviders' =>
   array (
@@ -124,8 +127,9 @@ cat <<EOF | sudo tee /var/www/html/nextcloud/config/autoconfig.php
     12 => 'OC\\Preview\\HEIF',
     13 => 'OC\\Preview\\TIFF',
     14 => 'OC\\Preview\\WEBP',
+    15 => 'OC\\Preview\\Image',
   ),
-
+  'logfile' => '/var/log/nextcloud.log',
 );
 EOF
 
@@ -134,68 +138,60 @@ chown www-data:www-data /var/nextcloud_data
 # Permisos a datos de usuario
 chmod 755 /var/nextcloud_data
 
-# Limpia los archivos innecesarios
-rm latest-27.zip
-
 # Damos permisos a la carpeta HTML para el usuario www-data
-sudo chown -R www-data:www-data /var/www/html/nextcloud/
-sudo chmod -R ug+rw /var/www/html/nextcloud/
-sudo chmod -R 770 /var/www/html/nextcloud/config
+chown -R www-data:www-data /var/www/html/nextcloud/
+chmod -R ug+rw /var/www/html/nextcloud/
+chmod -R 770 /var/www/html/nextcloud/config
 
-sudo -u www-data php /var/www/html/nextcloud/occ maintenance:install --database "mysql" --database-name "$DB_NAME" --database-user "$DB_USER" --database-pass "$DB_PASS" --admin-user "$NC_ADMIN_USER" --admin-pass "$NC_ADMIN_PASS" --data-dir /var/nextcloud_data
+su -s /bin/bash -c "php /var/www/html/nextcloud/occ maintenance:install --database=mysql --database-name=$DB_NAME --database-user=$DB_USER --database-pass=$DB_PASS --admin-user=$NC_ADMIN_USER --admin-pass=$NC_ADMIN_PASS --data-dir=/var/nextcloud_data" www-data
 
 ##### Configuraciones de nextcloud config.php y www.conf #####
 
-# Descomentamos las variables de www.conf
-sudo sed -i 's/;\(env\[HOSTNAME\] = \$HOSTNAME\)/\1/' /etc/php/${PHP_VERSION}/fpm/pool.d/www.conf
-sudo sed -i 's/;\(env\[PATH\] = \/usr\/local\/bin:\/usr\/bin:\/bin\)/\1/' /etc/php/${PHP_VERSION}/fpm/pool.d/www.conf
-sudo sed -i 's/;\(env\[TMP\] = \/tmp\)/\1/' /etc/php/${PHP_VERSION}/fpm/pool.d/www.conf
-sudo sed -i 's/;\(env\[TMPDIR\] = \/tmp\)/\1/' /etc/php/${PHP_VERSION}/fpm/pool.d/www.conf
-sudo sed -i 's/;\(env\[TEMP\] = \/tmp\)/\1/' /etc/php/${PHP_VERSION}/fpm/pool.d/www.conf
-
 # Habilitamos el modulo gmp de php
-sed -i 's/;extension=gmp/extension=gmp/' /etc/php/${PHP_VERSION}/fpm/php.ini
+sed -i 's/;extension=gmp/extension=gmp/' /etc/php/${PHP_VERSION}/apache2/php.ini
 # Se agrega el dominio a los host confiables de nextcloud
-sudo sed -i "s/'localhost',/'localhost',\n  1 => '$domain',/" /var/www/html/nextcloud/config/config.php
+sed -i "s/'localhost',/'localhost',\n  1 => '$domain',/" /var/www/html/nextcloud/config/config.php
 # Añadido para cloudflare (proxie inverso)
-sudo sed -i "/);/i \  'overwriteprotocol' => 'https'," /var/www/html/nextcloud/config/config.php
+sed -i "/);/i \  'overwriteprotocol' => 'https'," /var/www/html/nextcloud/config/config.php
 # overwrite.cli.url
-sudo sed -i "s/'overwrite.cli.url' => 'http:\/\/localhost',/'overwrite.cli.url' => 'https:\/\/$domain',/" /var/www/html/nextcloud/config/config.php
+sed -i "s/'overwrite.cli.url' => 'http:\/\/localhost',/'overwrite.cli.url' => 'https:\/\/$domain',/" /var/www/html/nextcloud/config/config.php
 # htaccess.RewriteBase
-sudo sed -i "/^);/i \ \ 'htaccess.RewriteBase' => '/'," /var/www/html/nextcloud/config/config.php
+sed -i "/^);/i \ \ 'htaccess.RewriteBase' => '/'," /var/www/html/nextcloud/config/config.php
 # Región telefónica
-sudo sed -i "/);/i \  'default_phone_region' => '$phone_region'," /var/www/html/nextcloud/config/config.php
+sed -i "/);/i \  'default_phone_region' => '$phone_region'," /var/www/html/nextcloud/config/config.php
+sed -i "/);/i \  'maintenance_window_start' => 1," /var/www/html/nextcloud/config/config.php
 
 # Configura la tarea cron para Nextcloud como usuario www-data
-sudo -u www-data crontab -l | { cat; echo "*/5 * * * * /usr/bin/php$PHP_VERSION /var/www/html/nextcloud/cron.php"; } | sudo -u www-data crontab -
+su -s /bin/bash -c "crontab -l | { cat; echo '*/5 * * * * php -f /var/www/html/nextcloud/cron.php'; } | crontab -u www-data -" www-data
+
 
 # Añade la configuración de fondo (background) a config.php
-sudo -u www-data php /var/www/html/nextcloud/occ config:system:set background --value=cron
+su -s /bin/bash -c "php /var/www/html/nextcloud/occ config:system:set background --value=cron" www-data
 
 
 # Configuración de Memcaches
-sudo sed -i "/);/i \  'memcache.local' => '\\\\OC\\\\Memcache\\\\Redis'," /var/www/html/nextcloud/config/config.php
-sudo sed -i "/);/i \  'memcache.distributed' => '\\\\OC\\\\Memcache\\\\Redis'," /var/www/html/nextcloud/config/config.php
+sed -i "/);/i \  'memcache.local' => '\\\\OC\\\\Memcache\\\\Redis'," /var/www/html/nextcloud/config/config.php
+sed -i "/);/i \  'memcache.distributed' => '\\\\OC\\\\Memcache\\\\Redis'," /var/www/html/nextcloud/config/config.php
 # redis
-sudo sed -i "/^);/i \  'redis' => [\n      'host' => 'localhost',\n      'port' => 6379,\n  ],\n  'memcache.locking' => '\\\\OC\\\\Memcache\\\\Redis'," /var/www/html/nextcloud/config/config.php
+sed -i "/^);/i \  'redis' => [\n      'host' => 'localhost',\n      'port' => 6379,\n  ],\n  'memcache.locking' => '\\\\OC\\\\Memcache\\\\Redis'," /var/www/html/nextcloud/config/config.php
 
 # Añadimos el usuario www-data al grupo redis
-sudo usermod -a -G redis www-data
+usermod -a -G redis www-data
 
 # Actualizamos el archivo .htaccess
-sudo -u www-data php /var/www/html/nextcloud/occ maintenance:update:htaccess
+su - www-data -s /bin/bash -c 'php /var/www/html/nextcloud/occ maintenance:update:htaccess'
+su - www-data -s /bin/bash -c 'php /var/www/html/nextcloud/occ db:add-missing-indices'
 
-# Cambiamos los valores de limitación de tamaño de archivos de php-fpm y user.ini
+# Cambiamos los valores de limitación de tamaño de archivos de php y user.ini
 
 echo "upload_max_filesize = 8192M" >> /var/www/html/nextcloud/.user.ini
 echo "post_max_size = 8192M" >> /var/www/html/nextcloud/.user.ini
-sed -i 's/memory_limit = 128M/memory_limit = 1024M/g' /etc/php/${PHP_VERSION}/fpm/php.ini
-sed -i 's/post_max_size = 8M/post_max_size = 8192M/g' /etc/php/${PHP_VERSION}/fpm/php.ini
-sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 8192M/g' /etc/php/${PHP_VERSION}/fpm/php.ini
-sed -i "s/output_buffering =.*/output_buffering = Off/g" /etc/php/${PHP_VERSION}/fpm/php.ini
-sed -i 's/max_execution_time = 30/max_execution_time = 3600/g' /etc/php/${PHP_VERSION}/fpm/php.ini
-sed -i 's/max_input_time = 60/max_input_time = 3600/g' /etc/php/${PHP_VERSION}/fpm/php.ini
-systemctl restart php${PHP_VERSION}-fpm.service
+sed -i 's/memory_limit = 128M/memory_limit = 1024M/g' /etc/php/${PHP_VERSION}/apache2/php.ini
+sed -i 's/post_max_size = 8M/post_max_size = 8192M/g' /etc/php/${PHP_VERSION}/apache2/php.ini
+sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 8192M/g' /etc/php/${PHP_VERSION}/apache2/php.ini
+sed -i "s/output_buffering =.*/output_buffering = Off/g" /etc/php/${PHP_VERSION}/apache2/php.ini
+sed -i 's/max_execution_time = 30/max_execution_time = 3600/g' /etc/php/${PHP_VERSION}/apache2/php.ini
+sed -i 's/max_input_time = 60/max_input_time = 3600/g' /etc/php/${PHP_VERSION}/apache2/php.ini
 
 # Valores de configuración de OPcache para Nextcloud
 opcache_settings="zend_extension=opcache
@@ -207,7 +203,7 @@ opcache.max_accelerated_files=10000
 opcache.revalidate_freq=0
 "
 
-echo "$opcache_settings" | sudo tee -a "/etc/php/"${PHP_VERSION}"/fpm/php.ini" >/dev/null
+echo "$opcache_settings" |  tee -a "/etc/php/"${PHP_VERSION}"/apache2/php.ini" >/dev/null
 
 # Redis
 redis_settings="redis.session.locking_enabled=1
@@ -215,11 +211,11 @@ redis.session.lock_retries=-1
 redis.session.lock_wait_time=10000
 "
 
-echo "$redis_settings" | sudo tee -a "/etc/php/"${PHP_VERSION}"/fpm/php.ini" >/dev/null
+echo "$redis_settings" |  tee -a "/etc/php/"${PHP_VERSION}"/apache2/php.ini" >/dev/null
 
+su - www-data -s /bin/bash -c 'php /var/www/html/nextcloud/occ db:add-missing-indices'
 
-sudo systemctl restart apache2
-sudo systemctl restart php${PHP_VERSION}-fpm.service
+service apache2 reload
 
 echo '\n'
 
